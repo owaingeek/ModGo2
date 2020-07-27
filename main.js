6922ade66fd14c53f8106122d794a037164bd74f
owaingeek <41023839+owaingeek@users.noreply.github.com>
2020-07-27 22:08:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,10 +12,35 @@ client.once('ready', () => {
 });
 
 client.on('message', msg => {
+    if (msg.content === 'IP') {
+      msg.reply('The IP is NetheriteCraft.serv.gs.');
+      msg.channel.send('Hey!! Please dont hate I am only a bot and I am still being deved. :sob:');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'Ip') {
+      msg.reply('The IP is NetheriteCraft.serv.gs.');
+      msg.channel.send('Hey!! Please dont hate I am only a bot and I am still being deved. :sob:');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'ip') {
+      msg.reply('The IP is NetheriteCraft.serv.gs.');
+      msg.channel.send('Hey!! Please dont hate I am only a bot and I am still being deved. :sob:');
+    }
+  });
+
+
+
+
+
+client.on('message', msg => {
     if (msg.content === 'owners') {
       msg.reply('The owners are 𝐸𝓋𝒶, 𝒯𝒽𝑒 𝒜𝓃𝒾𝓂𝑒 𝒻𝑜𝓍, Liam and OwainTehDevil');
       msg.channel.send('Hey!! Please dont hate I am only a bot and I am still being deved. :sob:');
     }
   });
 
-client.login(process.env.token);
+  client.login(process.env.token);
