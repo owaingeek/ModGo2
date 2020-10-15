220d8029676ccb5e431740016973b621291099b7
owaingeek <41023839+owaingeek@users.noreply.github.com>
2020-10-15 18:10:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -212,7 +212,7 @@ client.on('message', msg => {
     }
   });
   client.on('message', msg => {
-    if (msg.content === ';emojis') {
+    if (msg.content === ';listernerboost') {
      client.getMaxListeners(100);
       
     }
